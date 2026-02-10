03152cc9419d4e5b62e5e403cae235db8e910cbd
SajidKhan71 <sajid7271.sk@gmail.com>
2026-02-10 18:15:45 +0600

    added some meaningful comments

--- src/app/middleware/checkAuth.ts
+++ src/app/middleware/checkAuth.ts
@@ -8,94 +8,153 @@ import { prisma } from "../lib/prisma";
 import { CookieUtils } from "../utils/cookie";
 import { jwtUtils } from "../utils/jwt";
 
-export const checkAuth = (...authRoles: Role[]) => async (req: Request, res: Response, next: NextFunction) => {
+/**
+ * Middleware to check user authentication and authorization based on session token and access token.
+ *
+ * This middleware performs the following checks(shortly):
+ * 1. Verifies the presence and validity of the session token in cookies.
+ * 2. Checks if the user associated with the session is active and not deleted.
+ * 3. Verifies the presence and validity of the access token in cookies.
+ * 4. Checks if the user's role matches the required roles for accessing the resource.
+ *
+ * If any of the checks fail, an appropriate error response is sent back to the client.
+ */
+
+export const checkAuth =
+  (...authRoles: Role[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
     try {
-        //Session Token Verification
-        const sessionToken = CookieUtils.getCookie(req, "better-auth.session_token");
+      //!Session Token Verification
+      const sessionToken = CookieUtils.getCookie(
+        req,
+        // better auth e eivabe token thake
+        "better-auth.session_token",
+      );
 
-        if (!sessionToken) {
-            throw new Error('Unauthorized access! No session token provided.');
+      if (!sessionToken) {
+        throw new Error("Unauthorized access! No session token provided.");
+      }
+
+      /**
+       * Check if the session token exists in the database and is not expired. If valid, attach the user information to the request object for further processing. Also, check if the session is expiring soon and set appropriate headers for client-side handling.
+       */
+      if (sessionToken) {
+        const sessionExists = await prisma.session.findFirst({
+          where: {
+            token: sessionToken,
+            /// Ensure the session is still valid by checking the expiration time
+            expiresAt: {
+              gt: new Date(),
+            },
+          },
+          // we will extract user information from the session for further authorization checks and to attach it to the request object
+          include: {
+            user: true,
+          },
+        });
+
+        if (sessionExists && sessionExists.user) {
+          const user = sessionExists.user;
+
+          const now = new Date();
+          const expiresAt = new Date(sessionExists.expiresAt);
+          const createdAt = new Date(sessionExists.createdAt);
+
+          // Calculate the percentage of session life remaining and set headers if the session is expiring soon (less than 20% remaining).
+          const sessionLifeTime = expiresAt.getTime() - createdAt.getTime();
+          const timeRemaining = expiresAt.getTime() - now.getTime();
+          const percentRemaining = (timeRemaining / sessionLifeTime) * 100;
+
+          //! If the session is expiring soon, set custom headers to inform the client about the session status and remaining time.
+          if (percentRemaining < 20) {
+            res.setHeader("X-Session-Refresh", "true");
+            res.setHeader("X-Session-Expires-At", expiresAt.toISOString());
+            res.setHeader("X-Time-Remaining", timeRemaining.toString());
+
+            console.log("Session Expiring Soon!!");
+          }
+
+          // block or deleted user checking
+          if (
+            user.status === UserStatus.BLOCKED ||
+            user.status === UserStatus.DELETED
+          ) {
+            throw new AppError(
+              status.UNAUTHORIZED,
+              "Unauthorized access! User is not active.",
+            );
+          }
+
+          // Check if the user is marked as deleted in the database and prevent access if so.
+          if (user.isDeleted) {
+            throw new AppError(
+              status.UNAUTHORIZED,
+              "Unauthorized access! User is deleted.",
+            );
+          }
+
+          // If the user's role does not match any of the required roles for accessing the resource, throw a forbidden access error.
+          if (authRoles.length > 0 && !authRoles.includes(user.role)) {
+            throw new AppError(
+              status.FORBIDDEN,
+              "Forbidden access! You do not have permission to access this resource.",
+            );
+          }
+
+          req.user = {
+            userId: user.id,
+            role: user.role,
+            email: user.email,
+          };
         }
 
-        if (sessionToken) {
-            const sessionExists = await prisma.session.findFirst({
-                where: {
-                    token: sessionToken,
-                    expiresAt: {
-                        gt: new Date(),
-                    }
-                },
-                include: {
-                    user: true,
-                }
-            })
-
-            if (sessionExists && sessionExists.user) {
-                const user = sessionExists.user;
-
-                const now = new Date();
-                const expiresAt = new Date(sessionExists.expiresAt)
-                const createdAt = new Date(sessionExists.createdAt)
-
-                const sessionLifeTime = expiresAt.getTime() - createdAt.getTime();
-                const timeRemaining = expiresAt.getTime() - now.getTime();
-                const percentRemaining = (timeRemaining / sessionLifeTime) * 100;
-
-                if (percentRemaining < 20) {
-                    res.setHeader('X-Session-Refresh', 'true');
-                    res.setHeader('X-Session-Expires-At', expiresAt.toISOString());
-                    res.setHeader('X-Time-Remaining', timeRemaining.toString());
-
-                    console.log("Session Expiring Soon!!");
-                }
-
-                if (user.status === UserStatus.BLOCKED || user.status === UserStatus.DELETED) {
-                    throw new AppError(status.UNAUTHORIZED, 'Unauthorized access! User is not active.');
-                }
-
-                if (user.isDeleted) {
-                    throw new AppError(status.UNAUTHORIZED, 'Unauthorized access! User is deleted.');
-                }
-
-                if (authRoles.length > 0 && !authRoles.includes(user.role)) {
-                    throw new AppError(status.FORBIDDEN, 'Forbidden access! You do not have permission to access this resource.');
-                }
-
-                req.user = {
-                    userId : user.id,
-                    role : user.role,
-                    email : user.email,
-                }
-            }
-
-            const accessToken = CookieUtils.getCookie(req, 'accessToken');
-
-            if (!accessToken) {
-                throw new AppError(status.UNAUTHORIZED, 'Unauthorized access! No access token provided.');
-            }
-
-
-        }
-
-        //Access Token Verification
-        const accessToken = CookieUtils.getCookie(req, 'accessToken');
+        // we will checkt the access toekn for authorization and access control. If the access token is missing or invalid, we will throw an unauthorized access error.
+        const accessToken = CookieUtils.getCookie(req, "accessToken");
 
         if (!accessToken) {
-            throw new AppError(status.UNAUTHORIZED, 'Unauthorized access! No access token provided.');
+          throw new AppError(
+            status.UNAUTHORIZED,
+            "Unauthorized access! No access token provided.",
+          );
         }
+      }
 
-        const verifiedToken = jwtUtils.verifyToken(accessToken, envVars.ACCESS_TOKEN_SECRET);
+      //Access Token Verification
+      const accessToken = CookieUtils.getCookie(req, "accessToken");
 
-        if (!verifiedToken.success) {
-            throw new AppError(status.UNAUTHORIZED, 'Unauthorized access! Invalid access token.');
-        }
+      if (!accessToken) {
+        throw new AppError(
+          status.UNAUTHORIZED,
+          "Unauthorized access! No access token provided.",
+        );
+      }
 
-        if (authRoles.length > 0 && !authRoles.includes(verifiedToken.data!.role as Role)) {
-            throw new AppError(status.FORBIDDEN, 'Forbidden access! You do not have permission to access this resource.');
-        }
+      // verify the access token
+      const verifiedToken = jwtUtils.verifyToken(
+        accessToken,
+        envVars.ACCESS_TOKEN_SECRET,
+      );
 
-        next()
+      if (!verifiedToken.success) {
+        throw new AppError(
+          status.UNAUTHORIZED,
+          "Unauthorized access! Invalid access token.",
+        );
+      }
+
+      // If the user's role does not match any of the required roles for accessing the resource, throw a forbidden access error.
+      if (
+        authRoles.length > 0 &&
+        !authRoles.includes(verifiedToken.data!.role as Role)
+      ) {
+        throw new AppError(
+          status.FORBIDDEN,
+          "Forbidden access! You do not have permission to access this resource.",
+        );
+      }
+
+      next();
     } catch (error: any) {
-        next(error);
+      next(error);
     }
-};
+  };
--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -7,21 +7,31 @@ import AppError from "../errorHelpers/AppError";
 import { handleZodError } from "../errorHelpers/handleZodError";
 import { TErrorResponse, TErrorSources } from "../interfaces/error.interface";
 
-
+/**
+ * THIS is a global error handling middleware for the Express application. It captures and processes errors that occur during the request-response cycle, providing a centralized mechanism to handle different types of errors, including validation errors from Zod and custom application errors. The middleware formats the error response consistently and includes detailed information in development mode for easier debugging.
+ *
+ * The middleware checks the type of error and constructs an appropriate response based on whether it's a Zod validation error, a custom AppError, or a general Error. It also logs the error details in development mode for easier debugging and returns a structured JSON response with relevant information about the error.
+ */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    if (envVars.NODE_ENV === 'development') {
-        console.log("Error from Global Error Handler", err);
-    }
+export const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  if (envVars.NODE_ENV === "development") {
+    console.log("Error from Global Error Handler", err);
+  }
 
-    let errorSources: TErrorSources[] = []
-    let statusCode: number = status.INTERNAL_SERVER_ERROR;
-    let message: string = 'Internal Server Error';
-    let stack: string | undefined = undefined;
+  //! Error response structures
+  let errorSources: TErrorSources[] = [];
+  let statusCode: number = status.INTERNAL_SERVER_ERROR;
+  let message: string = "Internal Server Error";
+  let stack: string | undefined = undefined;
 
-    //Zod Error Patttern
-    /*
+  //Zod Error Patttern
+  /*
      error.issues; 
     /* [
       {
@@ -39,44 +49,48 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     ] 
     */
 
-    if (err instanceof z.ZodError) {
-        const simplifiedError = handleZodError(err);
-        statusCode = simplifiedError.statusCode as number
-        message = simplifiedError.message
-        errorSources = [...simplifiedError.errorSources]
-        stack = err.stack;
+  //! Custom Zod Error Pattern
+  if (err instanceof z.ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError.statusCode as number;
+    message = simplifiedError.message;
+    errorSources = [...simplifiedError.errorSources];
+    stack = err.stack;
+  }
+  //! Custom Application Error Pattern
+  else if (err instanceof AppError) {
+    statusCode = err.statusCode;
+    message = err.message;
+    stack = err.stack;
+    errorSources = [
+      {
+        path: "",
+        message: err.message,
+      },
+    ];
+  }
 
-    } else if (err instanceof AppError) {
-        statusCode = err.statusCode;
-        message = err.message;
-        stack = err.stack;
-        errorSources = [
-            {
-                path: '',
-                message: err.message
-            }
-        ]
-    }
-    else if (err instanceof Error) {
-        statusCode = status.INTERNAL_SERVER_ERROR;
-        message = err.message
-        stack = err.stack;
-        errorSources = [
-            {
-                path: '',
-                message: err.message
-            }
-        ]
-    }
+  //! General Error Pattern
+  else if (err instanceof Error) {
+    statusCode = status.INTERNAL_SERVER_ERROR;
+    message = err.message;
+    stack = err.stack;
+    errorSources = [
+      {
+        path: "",
+        message: err.message,
+      },
+    ];
+  }
 
+  //! Final response to send to the client with error details, including success status, error message, sources of the error, and optionally the error stack trace in development mode for easier debugging.
+  const errorResponse: TErrorResponse = {
+    success: false,
+    message: message,
+    errorSources,
+    error: envVars.NODE_ENV === "development" ? err : undefined,
+    stack: envVars.NODE_ENV === "development" ? stack : undefined,
+  };
 
-    const errorResponse: TErrorResponse = {
-        success: false,
-        message: message,
-        errorSources,
-        error: envVars.NODE_ENV === 'development' ? err : undefined,
-        stack: envVars.NODE_ENV === 'development' ? stack : undefined,
-    }
-
-    res.status(statusCode).json(errorResponse);
-}
+  res.status(statusCode).json(errorResponse);
+};
