921a83e80f2091b90e9cee5e8df445e4b2a0ff80
SajidKhan71 <sajid7271.sk@gmail.com>
2026-02-17 11:14:43 +0600

    part 5

--- src/app.ts
+++ src/app.ts
@@ -15,51 +15,64 @@ import { PaymentController } from "./app/module/payment/payment.controller";
 import { IndexRoutes } from "./app/routes";
 
 const app: Application = express();
-app.set("query parser", (str : string) => qs.parse(str));
+app.set("query parser", (str: string) => qs.parse(str));
 
 app.set("view engine", "ejs");
-app.set("views",path.resolve(process.cwd(), `src/app/templates`) )
+app.set("views", path.resolve(process.cwd(), `src/app/templates`));
 
-app.post("/webhook", express.raw({ type: "application/json" }), PaymentController.handleStripeWebhookEvent)
+app.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  PaymentController.handleStripeWebhookEvent,
+);
 
-app.use(cors({
-    origin : [envVars.FRONTEND_URL, envVars.BETTER_AUTH_URL, "http://localhost:3000", "http://localhost:5000"],
-    credentials : true,
-    methods : ["GET", "POST", "PUT", "DELETE", "PATCH"],
-    allowedHeaders : ["Content-Type", "Authorization"]
-}))
+app.use(
+  cors({
+    origin: [
+      envVars.FRONTEND_URL,
+      envVars.BETTER_AUTH_URL,
+      "http://localhost:3000",
+      "http://localhost:5000",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
-app.use("/api/auth", toNodeHandler(auth))
+app.use("/api/auth", toNodeHandler(auth));
 
 // Enable URL-encoded form data parsing
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to parse JSON bodies
+// Middleware to parse JSON bodiess
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 cron.schedule("*/25 * * * *", async () => {
-    try {
-        console.log("Running cron job to cancel unpaid appointments...");
-        await AppointmentService.cancelUnpaidAppointments();
-    } catch (error : any) {
-        console.error("Error occurred while canceling unpaid appointments:", error.message);    
-    }
-})
+  try {
+    console.log("Running cron job to cancel unpaid appointments...");
+    await AppointmentService.cancelUnpaidAppointments();
+  } catch (error: any) {
+    console.error(
+      "Error occurred while canceling unpaid appointments:",
+      error.message,
+    );
+  }
+});
 
 app.use("/api/v1", IndexRoutes);
 
 // Basic route
-app.get('/', async (req: Request, res: Response) => {
-    res.status(201).json({
-        success: true,
-        message: 'API is working',
-    })
+app.get("/", async (req: Request, res: Response) => {
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+  });
 });
 
-app.use(globalErrorHandler)
-app.use(notFound)
-
+app.use(globalErrorHandler);
+app.use(notFound);
 
 export default app;
